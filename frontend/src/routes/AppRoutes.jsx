9478266728b5a5c8183d2b842ede28543699ecfc
S.Bhavyasri Reddy <bhavyasri21205@gmail.com>
2025-09-06 22:54:01 +0530

    Milestone2

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,3 @@
-// src/routes/AppRoutes.jsx
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import HomePage from "../pages/HomePage";
