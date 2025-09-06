97dc517b37ab806f86f5710d398f5126a95f520b
S.Bhavyasri Reddy <bhavyasri21205@gmail.com>
2025-09-06 22:52:45 +0530

    Milestone2

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -58,7 +58,7 @@ export const login = async (req, res) => {
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "24h",
     });
 
     res.status(200).json({
